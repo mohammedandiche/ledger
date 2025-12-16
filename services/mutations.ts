c429b7e80a9873ec504cc278ccfbb60a9824c561
mohammedandiche <264603827+mohammedandiche@users.noreply.github.com>
2025-12-16 19:30:42 +0100

    feat(mutations): add mutations barrel module

---
+++ services/mutations.ts
@@ -0,0 +1,5 @@
+export * from './mutations/payeeMutationFields';
+export * from './mutations/accountMutationFields';
+export * from './mutations/transactionMutationFields';
+export * from './mutations/budgetMutationFields';
+export * from './mutations/categoryMutationFields';
