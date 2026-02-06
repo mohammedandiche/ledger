59e8907efbf2ba5ff19974d9aef58600e30ed466
mohammedandiche <264603827+mohammedandiche@users.noreply.github.com>
2026-02-06 20:08:22 +0100

    feat(form): add barrel exports for form components

---
+++ components/form/index.ts
@@ -0,0 +1,18 @@
+export {
+  useFormStyles,
+  makeFormStyles,
+  todayInt,
+  formatDateInt,
+  parseDateInput,
+} from './formStyles';
+export { FormRow } from './FormRow';
+export { PickerModal } from './PickerModal';
+export { PayeeInput } from './PayeeInput';
+export { CategoryPickerModal, SPLIT_CATEGORY } from './CategoryPickerModal';
+export { DatePickerModal } from './DatePickerModal';
+export { AmountSection } from './AmountSection';
+export { SplitChildCard } from './SplitChildCard';
+export { StatusPills } from './StatusPills';
+export { SaveOrSplitButton } from './SaveOrSplitButton';
+export { TransactionFormBody } from './TransactionFormBody';
+export type { SplitChild } from '@/hooks/useSplitForm';
